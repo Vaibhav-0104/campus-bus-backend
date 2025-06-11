83ea53c01ffab65481399b3c61e1b5dad5e2b755
Vaibhav-0104 <vaibhavsonar012@gmail.com>
2025-06-11 21:05:03 +0530

    Fresh initial commit

--- node-backend/config/firebase.js
+++ node-backend/config/firebase.js
@@ -7,20 +7,9 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Read Firebase credentials from JSON file
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.FIREBASE_CERT_URL,
-  universe_domain: "googleapis.com",
-};
-
+const serviceAccount = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../firebase-service-key.json"), "utf8")
+);
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
