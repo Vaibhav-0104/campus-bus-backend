7378f499300db357749534a7137827436c1c7f9e
Vaibhav-0104 <vaibhavsonar012@gmail.com>
2025-06-12 17:15:45 +0530

    updated port1

--- face-service/face_service.py
+++ face-service/face_service.py
@@ -214,4 +214,4 @@ def verify_face():
 
 if __name__ == "__main__":
     logger.info("🚀 Starting Flask face-service on port 8000")
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    app.run(host="0.0.0.0", port=8000, threaded=True)
